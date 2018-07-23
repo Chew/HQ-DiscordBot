aa7f30d90ebd06140c0b0d54dd3bfea2c4149092
Chewsterchew <chew@chew.pw>
2018-07-23 18:20:46 -0500

    hey let me know when it joins

--- lib/hq.rb
+++ lib/hq.rb
@@ -11,7 +11,7 @@ puts 'Config loaded from file'
 
 Bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'],
                                           client_id: CONFIG['client_id'],
-                                          prefix: ["<@#{CONFIG['client_id']}> ", CONFIG['prefix'], 'HQ, ']
+                                          prefix: ["<@#{CONFIG['client_id']}> ", 'hq, ', 'HQ, ', 'hq,', 'HQ,', 'hq', 'HQ']
 
 puts 'Initial Startup complete, loading all plugins...'
 
@@ -26,6 +26,36 @@ Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each do |wow|
   command = Object.const_get(command)
   Bot.include! command
   puts "Plugin #{command} successfully loaded!"
+end
+
+Bot.server_create do |event|
+  Bot.channel(471_092_848_238_788_608).send_embed do |e|
+    e.title = 'I did a join'
+
+    e.add_field(name: 'Server Name', value: event.server.name, inline: true)
+    e.add_field(name: 'Server ID', value: event.server.id, inline: true)
+    e.add_field(name: 'Server Count', value: event.bot.servers.count, inline: true)
+    e.add_field(name: 'User Count', value: event.server.members.count, inline: true)
+
+    userid = CONFIG['owner_id'].to_i
+    user = Bot.user(userid)
+
+    e.add_field(name: 'Are you on it?', value: event.server.members.include?(user), inline: true)
+
+    e.color = '00FF00'
+  end
+end
+
+Bot.server_delete do |event|
+  Bot.channel(471_092_848_238_788_608).send_embed do |e|
+    e.title = 'I did a leave'
+
+    e.add_field(name: 'Server Name', value: event.server.name, inline: true)
+    e.add_field(name: 'Server ID', value: event.server.id, inline: true)
+    e.add_field(name: 'Server Count', value: event.bot.servers.count, inline: true)
+
+    e.color = 'FF0000'
+  end
 end
 
 puts 'Done loading plugins! Finalizing start-up'

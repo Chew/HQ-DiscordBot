01b5c270d81357829c2a1bad35cf9bc25893e098
Chewsterchew <chew@chew.pw>
2018-07-23 17:19:57 -0500

    find user stats

---
+++ lib/plugins/user.rb
@@ -0,0 +1,54 @@
+module User
+  extend Discordrb::Commands::CommandContainer
+
+  command(:user, min_args: 0, max_args: 1) do |event, name|
+    name = event.user.nickname || event.user.name if name.nil?
+
+    findid = RestClient.get('https://api-quiz.hype.space/users',
+                            params: { q: name },
+                            Authorization: CONFIG['api'],
+                            'Content-Type': :json)
+
+    iddata = JSON.parse(findid)['data']
+
+    if iddata.length.zero?
+      begin
+        event.channel.send_embed do |embed|
+          embed.title = 'Error while searching for stats'
+          embed.colour = 'E6286E'
+          embed.description = 'Username not found.'
+        end
+      rescue Discordrb::Errors::NoPermission
+        event.respond 'That user doesn\'t exist!'
+      end
+    end
+
+    id = iddata[0]['userId']
+
+    data = RestClient.get("https://api-quiz.hype.space/users/#{id}",
+                          Authorization: CONFIG['api'],
+                          'Content-Type': :json)
+
+    data = JSON.parse(data)
+
+    begin
+      event.channel.send_embed do |embed|
+        embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "User stats for #{name}", url: data['referralUrl'])
+        embed.colour = '36399A'
+
+        embed.add_field(name: 'Game Stats', value: [
+          "Games Played - #{data['gamesPlayed']}",
+          "Win Count - #{data['winCount']}"
+        ].join("\n"), inline: false)
+
+        embed.add_field(name: 'Amount Won:', value: data['leaderboard']['total'])
+
+        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Account created on')
+        embed.timestamp = Time.parse(data['created'])
+        embed.thumbnail = { url: data['avatarUrl'].to_s }
+      end
+    rescue Discordrb::Errors::NoPermission
+      event.respond 'Hey, Scott Rogowski here. I need some memes, dreams, and the ability to embed links! You gotta grant me these permissions!'
+    end
+  end
+end

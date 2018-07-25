43e81580c8bb19f2a5c17ea4a970e72291426dab
Chewsterchew <chew@chew.pw>
2018-07-24 21:30:12 -0500

    improvements to next, shows if live

--- lib/plugins/nextgame.rb
+++ lib/plugins/nextgame.rb
@@ -9,15 +9,46 @@ module RandomQuestion
 
     data = JSON.parse(data)
 
+    active = data['active']
+
+    kind = if active
+             if data['vertical'] == 'general'
+               'Normal Show'
+             elsif data['vertical'] == 'sports'
+               'Sports Show'
+             else
+               'Unknown Show'
+             end
+           else
+             if data['upcoming'][0]['vertical'] == 'general'
+               'Normal Show'
+             elsif data['upcoming'][0]['vertical'] == 'sports'
+               'Sports Show'
+             else
+               'Unknown Show'
+             end
+           end
+
+    prize = if active
+              data['prize'].to_s
+            else
+              data['upcoming'][0]['prize']
+            end
+
     begin
       event.channel.send_embed do |embed|
-        embed.title = 'Next HQ Game'
-        embed.colour = '36399A'
+        if active
+          embed.title = 'HQ is Live!'
+          embed.colour = 'E6286E'
+        else
+          embed.title = 'Upcoming HQ Game'
+          embed.colour = '36399A'
+          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Show Time')
+          embed.timestamp = Time.parse(data['upcoming'][0]['time'])
+        end
 
-        embed.add_field(name: 'Prize:', value: data['upcoming'][0]['prize'])
-
-        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Show Time')
-        embed.timestamp = Time.parse(data['upcoming'][0]['time'])
+        embed.add_field(name: 'Prize:', value: prize, inline: true)
+        embed.add_field(name: 'Type', value: kind, inline: true)
       end
     rescue Discordrb::Errors::NoPermission
       event.respond 'Hey, Scott Rogowski here. I need some memes, dreams, and the ability to embed links! You gotta grant me these permissions!'

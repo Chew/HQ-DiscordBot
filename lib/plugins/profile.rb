be940b249c66e58b8e8860e6025e88ba4beb6f57
Chewsterchew <chew@chew.pw>
2018-07-29 21:44:44 -0500

    Profile system, set your username and region!

---
+++ lib/plugins/profile.rb
@@ -0,0 +1,49 @@
+module Profile
+  extend Discordrb::Commands::CommandContainer
+
+  command(:profile) do |event|
+    filename = "profiles/#{event.user.id}.yaml"
+    unless File.exist?(filename)
+      File.new(filename, 'w+')
+      exconfig = YAML.load_file('profiles/0.yaml')
+      exconfig['username'] = event.user.nickname || event.user.name
+      File.open(filename, 'w') { |f| f.write exconfig.to_yaml }
+    end
+
+    data = YAML.load_file(filename)
+
+    event.channel.send_embed do |embed|
+      embed.title = "HQBot Profile for #{event.user.name}"
+      embed.colour = '36399A'
+
+      embed.add_field(name: 'HQ Username', value: data['username'], inline: true)
+      embed.add_field(name: 'Region', value: data['region'], inline: true)
+
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Change with: hq, set [type] [option]')
+    end
+  end
+
+  command(:set) do |event, type, setting|
+    filename = "profiles/#{event.user.id}.yaml"
+    unless File.exist?(filename)
+      File.new(filename, 'w+')
+      exconfig = YAML.load_file('profiles/0.yaml')
+      exconfig['username'] = event.user.nickname || event.user.name
+      File.open(filename, 'w') { |f| f.write exconfig.to_yaml }
+    end
+
+    data = YAML.load_file(filename)
+
+    case type.downcase
+    when 'username', 'region'
+      data[type.downcase] = setting
+    else
+      event.respond 'Invalid type!'
+      break
+    end
+
+    File.open(filename, 'w') { |f| f.write data.to_yaml }
+
+    event.respond ":+1: Successfully set `#{type}` to `#{setting}`"
+  end
+end

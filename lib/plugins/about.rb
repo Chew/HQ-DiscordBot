9a9bb4a100455544c179c01ec466fb564db6c5de
Chewsterchew <chew@chew.pw>
2018-08-10 10:14:24 -0500

    add more used libraries

--- lib/plugins/about.rb
+++ lib/plugins/about.rb
@@ -104,7 +104,7 @@ module About
 
   command(:lib) do |event|
     gems = `gem list`.split("\n")
-    libs = ['discordrb', 'rest-client', 'json', 'yaml', 'nokogiri']
+    libs = ['discordrb', 'rest-client', 'json', 'yaml', 'nokogiri', 'dblruby', 'listcordrb']
     versions = []
     libs.each do |name|
       version = gems[gems.index { |s| s.include?(name) }].split(' ')[1]

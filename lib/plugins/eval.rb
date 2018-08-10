4405ecd50f4086954bfd4f2baff39c03eed9f341
Chewsterchew <chew@chew.pw>
2018-08-10 17:38:36 -0500

    add eval command (lord help us all)

---
+++ lib/plugins/eval.rb
@@ -0,0 +1,24 @@
+module Eval
+  extend Discordrb::Commands::CommandContainer
+
+  command(:eval) do |event, *code|
+    break unless event.user.id == CONFIG['owner_id']
+    begin
+      event.channel.send_embed do |e|
+        e.title = '**Evaluated Successfully**'
+
+        evaluated = eval code.join(' ')
+
+        e.description = evaluated
+        e.color = '00FF00'
+      end
+    rescue StandardError => f
+      event.channel.send_embed do |e|
+        e.title = '**Evaluation Failed!**'
+
+        e.description = f.to_s
+        e.color = 'FF0000'
+      end
+    end
+  end
+end

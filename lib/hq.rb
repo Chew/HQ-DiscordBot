0e6dd250088fa426f9763659505f7a753205da5f
Chewsterchew <chew@chew.pw>
2018-08-11 07:06:44 -0600

    add a reload command

--- lib/hq.rb
+++ lib/hq.rb
@@ -26,15 +26,26 @@ puts 'Initial Startup complete, loading all plugins...'
 
 Starttime = Time.now
 
-Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
+def loadpls
+  Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each do |wow|
+    load wow
+    require wow
+    bob = File.readlines(wow) { |line| line.split.map(&:to_s).join }
+    command = bob[0][7..bob[0].length]
+    command.delete!("\n")
+    command = Object.const_get(command)
+    Bot.include! command
+    puts "Plugin #{command} successfully loaded!"
+  end
+end
 
-Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each do |wow|
-  bob = File.readlines(wow) { |line| line.split.map(&:to_s).join }
-  command = bob[0][7..bob[0].length]
-  command.delete!("\n")
-  command = Object.const_get(command)
-  Bot.include! command
-  puts "Plugin #{command} successfully loaded!"
+loadpls
+
+Bot.command(:reload) do |event|
+  break unless event.user.id == CONFIG['owner_id']
+  Bot.clear!
+  loadpls
+  event.respond "Reloaded sucessfully!"
 end
 
 Bot.server_create do |event|

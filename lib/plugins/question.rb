856eb9b88fd1e14e8ca836875c6800154a531feb
Chewsterchew <chew@chew.pw>
2018-08-19 08:58:48 -0500

    hq, question improvements

--- lib/plugins/question.rb
+++ lib/plugins/question.rb
@@ -1,20 +1,30 @@
 module Question
   extend Discordrb::Commands::CommandContainer
 
-  command(:question) do |event|
+  command(:question) do |event, delay = 10|
+    delay = delay.to_i
+    if delay > 60
+      msg = 'What kind of question has a longer than minute time to answer the question?'
+      delay = 60
+    elsif delay.zero? || delay == -1
+      msg = 'Answer will not be revealed...'
+    elsif delay < 2
+      msg = 'What kind of question gives you less than 2 seconds to answer it?'
+      delay = 2
+    end
     filename = "profiles/#{event.user.id}.yaml"
     if File.exist?(filename)
       profile = YAML.load_file(filename)
       if profile['donator'].nil? || profile['donator'] == false
-        event.respond "Hey, Scott Rogowsky here, because this command is in beta, only donators may use this command, but it will be available to everyone very soon!"
+        event.respond 'Hey, Scott Rogowsky here, because this command is in beta, only donators may use this command, but it will be available to everyone very soon!'
         break
       end
     else
-      event.respond "Hey, Scott Rogowsky here, because this command is in beta, only donators may use this command, but it will be available to everyone very soon!"
+      event.respond 'Hey, Scott Rogowsky here, because this command is in beta, only donators may use this command, but it will be available to everyone very soon!'
       break
     end
     data = JSON.parse(RestClient.get('http://api.chew.pro/hq/random'))
-    m = event.channel.send_embed do |embed|
+    m = event.channel.send_embed(msg) do |embed|
       embed.title = data['question']['question']
       embed.colour = 0x66e8a9
       embed.description = [
@@ -24,30 +34,36 @@ module Question
       ].join("\n")
 
       embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: 'HQ Random Question')
-      embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Answer will be revealed in 10 seconds...')
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Answer will be revealed in ' + delay.to_s + ' seconds...') unless delay.zero? || delay == -1
     end
-    sleep 10
+    if delay.zero? || delay == -1
+      break
+    else
+      sleep delay
+    end
     cor = data['question']['correct']
-    c1 = data['question']['choice1'] + " - " + data['choices']['picked1']
-    c2 = data['question']['choice2'] + " - " + data['choices']['picked2']
-    c3 = data['question']['choice3'] + " - " + data['choices']['picked3']
+    c1 = data['question']['choice1'] + ' - ' + data['choices']['picked1']
+    c2 = data['question']['choice2'] + ' - ' + data['choices']['picked2']
+    c3 = data['question']['choice3'] + ' - ' + data['choices']['picked3']
     case cor
-    when "1"
-      c1 = ":heavy_check_mark:" + c1
-      c2 = ":x:" + c2
-      c3 = ":x:" + c3
-    when "2"
-      c1 = ":x:" + c1
-      c2 = ":heavy_check_mark:" + c2
-      c3 = ":x:" + c3
-    when "3"
-      c1 = ":x:" + c1
-      c2 = ":x:" + c2
-      c3 = ":heavy_check_mark:" + c3
+    when '1'
+      c1 = ':heavy_check_mark:' + c1
+      c2 = ':x:' + c2
+      c3 = ':x:' + c3
+    when '2'
+      c1 = ':x:' + c1
+      c2 = ':heavy_check_mark:' + c2
+      c3 = ':x:' + c3
+    when '3'
+      c1 = ':x:' + c1
+      c2 = ':x:' + c2
+      c3 = ':heavy_check_mark:' + c3
     end
     m.edit('', Discordrb::Webhooks::Embed.new(
-                 title:  data['question']['question'],
-                 description: [c1,c2,c3].join("\n")
+                 title: data['question']['question'],
+                 description: [c1, c2, c3].join("\n"),
+                 footer: {text: 'This question was asked:'},
+                 timestamp: Time.parse(data['game']['time'])
                ))
   end
 end

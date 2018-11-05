61c75a252a656e90d2a10223f6302fab0cb3dccd
Chewsterchew <chew@chew.pw>
2018-11-05 16:33:05 -0600

    Move some of the load from bot.rb to run.rb

--- lib/hq.rb
+++ lib/hq.rb
@@ -1,46 +1,10 @@
-require 'discordrb'
-require 'rest-client'
-require 'json'
-require 'yaml'
-require 'open-uri'
-require 'dblruby'
-require 'mysql2'
-puts 'All dependencies loaded'
-
-CONFIG = YAML.load_file('config.yaml')
-puts 'Config loaded from file'
-
-DBL = DBLRuby.new(CONFIG['dbotsorg'], CONFIG['client_id'])
-puts 'Properly Instantiated DBL!'
-
-begin
-  DB = Mysql2::Client.new(
-    host: CONFIG['db']['host'],
-    username: CONFIG['db']['username'],
-    password: CONFIG['db']['password'],
-    database: CONFIG['db']['database']
-  )
-rescue Mysql2::Error::ConnectionError
-  puts 'Unable to connect to the database. Good going!'
-  exit
-end
-
-puts 'Connected to database'
+puts 'Initial Startup complete, loading all plugins...'
 
 require_relative 'extensions/dbgeek'
 require_relative 'extensions/commandz'
 require_relative 'extensions/botuser'
 
 DBHelper = DbGeek.new
-
-prefixes = ["<@#{CONFIG['client_id']}> ", 'hq, ', 'HQ, ', 'hq,', 'HQ,', 'hq', 'HQ', 'Hq, ', 'Hq ', 'Hq', 'Hq,'].freeze
-
-Bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'],
-                                          client_id: CONFIG['client_id'],
-                                          prefix: prefixes,
-                                          ignore_bots: true
-
-puts 'Initial Startup complete, loading all plugins...'
 
 Commands = Commandz.new
 
@@ -63,6 +27,7 @@ loadpls
 
 Bot.command(:reload) do |event|
   break unless event.user.id == CONFIG['owner_id']
+
   Bot.clear!
   loadpls
   event.respond 'Reloaded sucessfully!'
@@ -100,11 +65,11 @@ Bot.server_delete do |event|
   end
 end
 
-#Bot.message(starts_with: /prefixes.join('|')/) do |event|
-# Commands.add
-#  puts "Command ran by #{event.user.distinct} (#{event.user.id}): #{event.message.content}"
-#  nil
-#end
+Bot.message(contains: /'hq, '/) do |event|
+  Commands.add
+  puts "Command ran by #{event.user.distinct} (#{event.user.id}): #{event.message.content}"
+  nil
+end
 
 puts 'Done loading plugins! Finalizing start-up'
 
--- run.rb
+++ run.rb
@@ -1,3 +1,45 @@
 puts "Let's start that bot!"
 
+ARGV.each do |e|
+  puts "Argument found: #{e}"
+end
+
+require 'discordrb'
+require 'rest-client'
+require 'json'
+require 'yaml'
+require 'open-uri'
+require 'dblruby'
+require 'mysql2'
+puts 'All dependencies loaded'
+
+CONFIG = YAML.load_file('config.yaml')
+puts 'Config loaded from file'
+
+DBL = DBLRuby.new(CONFIG['dbotsorg'], CONFIG['client_id'])
+puts 'Properly Instantiated DBL!'
+
+begin
+  DB = Mysql2::Client.new(
+    host: CONFIG['db']['host'],
+    username: CONFIG['db']['username'],
+    password: CONFIG['db']['password'],
+    database: CONFIG['db']['database']
+  )
+rescue Mysql2::Error::ConnectionError
+  puts 'Unable to connect to the database. Good going!'
+  exit
+end
+
+puts 'Connected to database'
+
+prefixes = ["<@#{CONFIG['client_id']}> ", 'hq, ', 'HQ, ', 'hq,', 'HQ,', 'hq', 'HQ', 'Hq, ', 'Hq ', 'Hq', 'Hq,'].freeze
+
+Bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'],
+                                          client_id: CONFIG['client_id'],
+                                          prefix: prefixes,
+                                          ignore_bots: true,
+                                          num_shards: CONFIG['shards'],
+                                          shard_id: ARGV[0].to_i
+
 require_relative 'lib/hq'

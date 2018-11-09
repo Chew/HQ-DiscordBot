9af53ddd39be732663ca12fd38dc4fd38e4e6d4a
Chewsterchew <chew@chew.pw>
2018-11-09 14:13:42 -0600

    Add hq, answer and hq, epic

--- lib/plugins/extra.rb
+++ lib/plugins/extra.rb
@@ -2,10 +2,18 @@ module Extra
   extend Discordrb::Commands::CommandContainer
 
   command(:dab) do |event|
-    event.respond 'Dab!!!!!1111!!! epic gamer win'
+    event.respond 'Dabbed!111!!11!!!1!!11!!11!!11!! epic gamer win'
   end
 
   command(:life) do |event|
     event.respond 'In order to generate extra lives, we need some information. First, we need your credit card number, then the 3 numbers on the back, the expiration month AND year, and your username. You are guaranteed to get lives after we receive this information! Also ignore the possible credit card scam alert emails.'
+  end
+
+  command(:answer) do |event|
+    event.respond 'The answer is always 3 if Scott or Beric are hosting, always 2 if Matt is hosting, and if someone else is hosting just guess.'
+  end
+
+  command(:epic) do |event|
+    event.respond 'Okay, now this is epic.'
   end
 end

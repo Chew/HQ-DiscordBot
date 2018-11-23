6d840cfa352c7c781fc8547a571e8cb6cec14adc
Chewsterchew <chew@chew.pw>
2018-11-23 15:56:55 -0600

    Re-wrote hq, next. Much better and more specific now.

--- lib/plugins/nextgame.rb
+++ lib/plugins/nextgame.rb
@@ -1,77 +1,91 @@
 module NextGame
   extend Discordrb::Commands::CommandContainer
 
-  command(:nextgame, aliases: [:next]) do |event, type = 'us'|
+  command(:nextgame, aliases: [:next]) do |event, *stuff|
+    stuff = stuff.join(' ').downcase
+
+    region = if stuff.include?('us')
+               'us'
+             elsif stuff.include?('uk')
+               'uk'
+             end
+
+    kind = []
+    kind.push 'general'
+    kind.delete 'general' if stuff.include?('sports') || stuff.include?('words')
+    kind.push 'sports' if stuff.include? 'sports'
+    kind.push 'words' if stuff.include? 'words'
+
     dbuser = BotUser.new(event.user.id)
-    if dbuser.exists? && type.nil?
-      type = dbuser.type
-    elsif type.nil?
-      type = 'us'
+    if dbuser.exists? && region.nil?
+      region = dbuser.region
+    elsif region.nil?
+      region = 'us'
     end
 
-    stk = case type.downcase
+    stk = case region.downcase
           when 'uk'
             'Mg=='
           else
             'MQ=='
           end
 
-    data = if type.downcase.include?('word')
-             RestClient.get('https://api-quiz.hype.space/shows/now',
-                            Authorization: CONFIG['api'],
-                            'x-hq-client': 'iOS/1.3.27 b121',
-                            'Content-Type': :json)
-           else
-             RestClient.get('https://api-quiz.hype.space/shows/now',
-                            params: { type: 'hq' },
-                            'x-hq-stk': stk,
-                            'Content-Type': :json)
-           end
+    data = RestClient.get('https://api-quiz.hype.space/shows/schedule',
+                          Authorization: CONFIG['api'],
+                          'x-hq-stk': stk,
+                          'x-hq-client': 'iOS/1.3.27 b121',
+                          'Content-Type': :json)
 
     data = JSON.parse(data)
 
-    active = data['active']
+    # active = data['active']
 
-    kind = if active
-             if data['vertical'] == 'general'
-               'Normal'
-             elsif data['vertical'] == 'sports'
-               'Sports'
-             elsif data['vertical'] == 'words'
-               'Words'
-             else
-               'Unknown'
-             end
-           elsif data['upcoming'][0]['vertical'] == 'general'
-             'Normal'
-           elsif data['upcoming'][0]['vertical'] == 'sports'
-             'Sports'
-           elsif data['upcoming'][0]['vertical'] == 'words'
-             'Words'
-           else
-             'Unknown'
-           end
+    showstuff = ''
 
-    prize = if active
-              data['prize'].to_s
-            else
-              data['upcoming'][0]['prize']
-            end
+    data['shows'].each do |show|
+      next unless showstuff == ''
+      showstuff = show if kind.include? show['vertical']
+    end
+
+    currency = if showstuff['currency'] == 'USD'
+                 '$'
+               elsif showstuff['currency'] == 'GBP'
+                 '£'
+               end
+
+    begin
+      prize = (showstuff['prizeCents'] / 100).to_s
+    rescue NoMethodError
+      begin
+        event.channel.send_embed do |embed|
+          embed.title = 'No Games Match Given Criteria'
+          embed.colour = 'E6286E'
+        end
+      rescue Discordrb::Errors::NoPermission
+        event.respond 'No Games Match Given Criteria!'
+      end
+      break
+    end
+
+    gametype = if showstuff['vertical'] == 'general'
+                 'Trivia'
+               elsif showstuff['vertical'] == 'sports'
+                 'Sports'
+               elsif showstuff['vertical'] == 'words'
+                 'Words'
+               else
+                 'Unknown'
+               end
 
     begin
       event.channel.send_embed do |embed|
-        if active
-          embed.title = 'HQ is Live!'
-          embed.colour = 'E6286E'
-        else
-          embed.title = 'Upcoming HQ Game'
-          embed.colour = '36399A'
-          embed.footer = { text: 'Show Time' }
-          embed.timestamp = Time.parse(data['upcoming'][0]['time'])
-        end
+        embed.title = 'Upcoming HQ Game'
+        embed.colour = '36399A'
+        embed.footer = { text: 'Show Time' }
+        embed.timestamp = Time.parse(showstuff['startTime'])
 
-        embed.add_field(name: 'Prize', value: prize, inline: true)
-        embed.add_field(name: 'Type', value: kind, inline: true)
+        embed.add_field(name: 'Prize', value: currency + prize, inline: true)
+        embed.add_field(name: 'Type', value: gametype, inline: true)
       end
     rescue Discordrb::Errors::NoPermission
       event.respond 'Hey, Scott Rogowsky here. I need some memes, dreams, and the ability to embed links! You gotta grant me these permissions!'

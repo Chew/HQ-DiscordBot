d7ffb103abc224f7a5d669cd51d8140157d55c49
Chew <chew@chew.pw>
2018-11-28 16:48:24 -0600

    Add missing quotation mark

--- lib/hq.rb
+++ lib/hq.rb
@@ -80,7 +80,7 @@ Bots.each do |bot|
 
   puts 'Done loading plugins! Finalizing start-up'
 
-  hosts = ["Scott Rogowsky", "Matt Richards", "Sarah Pribis", "David Magidoff", "Tyler West", "Lauren Gambino", "Sharon Carpenter", "Beric Livingstone", "Emma Tattenbaum", Anna Roisman", "Sian Welby", "Leonie Zeumer", "Lara Falkner", "Kathryn Goldsmith", "Jimmy Kimmel", "Charlie O'Connor", "Alexandra Maurer"]
+  hosts = ["Scott Rogowsky", "Matt Richards", "Sarah Pribis", "David Magidoff", "Tyler West", "Lauren Gambino", "Sharon Carpenter", "Beric Livingstone", "Emma Tattenbaum", "Anna Roisman", "Sian Welby", "Leonie Zeumer", "Lara Falkner", "Kathryn Goldsmith", "Jimmy Kimmel", "Charlie O'Connor", "Alexandra Maurer"]
 
   bot.ready do |_event|
     bot.game = "with #{hosts.sample}! | hq, help"

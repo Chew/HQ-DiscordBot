574ba3f2da7c721991fca0fe298f900cec7b0b29
Chewsterchew <chew@chew.pw>
2018-12-20 11:22:26 -0600

    Major leaderboard improvement, get words, more pages, and more!

--- lib/plugins/leaderboard.rb
+++ lib/plugins/leaderboard.rb
@@ -1,17 +1,41 @@
 module Leaderboard
   extend Discordrb::Commands::CommandContainer
 
-  command(:leaderboard, aliases: %i[lb leader rank ranks], min_args: 0, max_args: 1) do |event, type = 1|
-    if type == 'weekly'
-      type = 1
-    elsif ['all-time', 'all', 'alltime'].include? type
-      type = 0
+  command(:leaderboard, aliases: %i[lb leader rank ranks], min_args: 0) do |event, *args|
+    stuff = args.join(' ').downcase
+
+    page = 1
+
+    args.each do |e|
+      page = e.to_i if e.to_i != 0
     end
-    type = type.to_i
+
+    type = if stuff.include?('all')
+             0
+           else
+             1
+           end
+
+    region = if stuff.include?('us')
+               ['US', 'United States', 'MQ==']
+             elsif stuff.include?('uk')
+               ['UK', 'United Kingdom', 'Mg==']
+             elsif stuff.include?('de')
+               ['DE', 'Germany', 'Mw==']
+             elsif stuff.include?('au')
+               ['AU', 'Australia', 'NA==']
+             else
+               ['US', 'United States', 'MQ==']
+             end
+
+    words = stuff.include?('words')
+
+    game = ('hq-words' if words)
 
     leaders = RestClient.get('https://api-quiz.hype.space/users/leaderboard',
-                             params: { mode: type },
+                             params: { mode: type, type: game },
                              Authorization: CONFIG['api'],
+                             'x-hq-stk': region[2],
                              'Content-Type': :json)
 
     leaders = JSON.parse(leaders)
@@ -28,8 +52,8 @@ module Leaderboard
 
     output = []
 
-    10.times do |excuse|
-      output[excuse] = "\##{excuse + 1}: #{leadernames[excuse]} - $#{leadermoney[excuse]} (#{leaderwins[excuse]} wins)"
+    100.times do |excuse|
+      output.push "\##{excuse + 1}: #{leadernames[excuse]} - $#{leadermoney[excuse]} (#{leaderwins[excuse]} wins)"
     end
 
     tie = if type.zero?
@@ -38,11 +62,13 @@ module Leaderboard
             'Weekly'
           end
 
+    wordshow = 'Words ' if words
+
     begin
       event.channel.send_embed do |embed|
-        embed.title = 'Top 10 Leaderboards for ' + tie
+        embed.title = "Leaderboards for #{wordshow}#{region[1]} #{tie} (page #{page})"
         embed.colour = '36399A'
-        embed.description = output.join("\n")
+        embed.description = output[0 + ((page - 1) * 10)..9 + ((page - 1) * 10)].join("\n")
       end
     rescue Discordrb::Errors::NoPermission
       event.respond 'Hey, Scott Rogowsky here. I need some memes, dreams, and the ability to embed links! You gotta grant me these permissions!'

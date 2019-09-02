188907afb026b0617a5a75af877a1ca62b5d8f77
Chew <chew@chew.pw>
2019-09-02 11:36:32 -0500

    expired keys for chew

--- lib/plugins/authkey.rb
+++ lib/plugins/authkey.rb
@@ -3,17 +3,17 @@ module AuthKey
 
   command(:authkey, min_args: 0) do |event, *args|
     unless event.channel.pm?
-      event.respond "This command only works in DMs!"
+      event.respond 'This command only works in DMs!'
       event.message.delete
       break
     end
 
     if args.empty?
-      event.respond "No key specified, make sure you get a key from <https://chew.pw/hqbot/authkey>"
+      event.respond 'No key specified, make sure you get a key from <https://chew.pw/hqbot/authkey>'
       break
     end
 
-    m = event.respond "<a:loading:393852367751086090> Checking your profile..."
+    m = event.respond '<a:loading:393852367751086090> Checking your profile...'
     user = BotUser.new(event.user.id)
     unless user.exists?
       m.edit "<:xmark:314349398824058880> You don't have a profile! Type `hq, profile` and try again."
@@ -21,11 +21,9 @@ module AuthKey
     end
     m.edit "<:check:314349398811475968> Profile found!\n<a:loading:393852367751086090> Checking to see if key is valid..."
     key = args.join(' ')
-    unless key.include?('Bearer ')
-      key = "Bearer #{key}"
-    end
+    key = "Bearer #{key}" unless key.include?('Bearer ')
     begin
-      testing = HT.get("users/me", key)
+      testing = HT.get('users/me', key)
     rescue RestClient::Unauthorized
       m.edit "<:check:314349398811475968> Profile found!\n<:xmark:314349398824058880> Key is invalid! Did you copy it correctly?"
       break
@@ -40,15 +38,15 @@ module AuthKey
     DBHelper.updateuser(event.user.id, 'authkey', 1)
     DBHelper.updateuser(event.user.id, 'keyid', testing['userId'])
 
-    memb = Bot.server(463178169105645569).member(event.user.id)
+    memb = Bot.server(463_178_169_105_645_569).member(event.user.id)
     if memb.nil?
       extra = "\n\nMake sure you join the official server for a role! https://discord.gg/ugryabu"
     else
-      memb.add_role(473301704272773120)
+      memb.add_role(473_301_704_272_773_120)
     end
 
-    Bot.channel(618115005346217986).send_embed do |embed|
-      embed.title = "New Authkey Donor!"
+    Bot.channel(618_115_005_346_217_986).send_embed do |embed|
+      embed.title = 'New Authkey Donor!'
       embed.add_field(name: 'User', value: [event.user.distinct, event.user.id, event.user.mention].join("\n"), inline: true)
       embed.add_field(name: 'On Server?', value: !memb.nil?, inline: true)
     end
@@ -66,8 +64,26 @@ module AuthKey
 
     decoded_token = JWT.decode keys[user.keyid].gsub('Bearer ', ''), nil, false
     event.channel.send_embed do |embed|
-      embed.description = "Your key will expire at"
+      embed.description = 'Your key will expire at'
       embed.timestamp = Time.at(decoded_token[0]['exp'])
+    end
+  end
+
+  command(:expiredkeys) do |event|
+    break unless event.user.id == CONFIG['owner_id']
+
+    keys = JSON.parse(File.read('keys.json'))
+
+    gone = []
+
+    keys.each do |key, data|
+      decoded_token = JWT.decode data.gsub('Bearer ', ''), nil, false
+      expire = Time.at(decoded_token[0]['exp'])
+      gone.push(key) if expire < Time.now
+    end
+
+    event.channel.send_embed do |embed|
+      embed.description = "Expired Key IDs\n#{gone.join("\n")}"
     end
   end
 end
